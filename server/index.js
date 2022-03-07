fd54e75ded73d35d2e9ebffcc06ffe94447de02e
baoanh1310 <dataholicguy@gmail.com>
2022-03-08 04:24:15 +0700

    use CORS

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 // Routes
 const authRouter = require('./routes/auth')
@@ -27,6 +28,7 @@ connectDB()
 
 const app = express()
 app.use(express.json())
+app.use(cors())
 
 app.use('/api/auth', authRouter)
 app.use('/api/posts', postRouter)
